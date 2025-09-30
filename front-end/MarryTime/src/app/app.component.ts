0530d25825e88ae830863f9b20929d2bb95738b9
Abdallah Taha <abdallahkhoder59@gmail.com>
2025-09-30 19:17:41 +0300

    add environments

--- front-end/MarryTime/src/app/app.component.ts
+++ front-end/MarryTime/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { ComponentComponent } from "./dashboard/sections/component/component.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, FooterComponent, RouterOutlet],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet, ComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
