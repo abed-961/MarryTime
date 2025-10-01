2a2e4b9cc85a44c08f02cfe490274c211306893b
Abdallah Taha <abdallahkhoder59@gmail.com>
2025-10-01 19:16:43 +0300

    edit on some style

--- front-end/MarryTime/src/app/layout/footer/footer.component.ts
+++ front-end/MarryTime/src/app/layout/footer/footer.component.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { discardPeriodicTasks } from '@angular/core/testing';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  style: any = {};
 
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url === '/user/login') {
+          this.style = {
+            position: 'fixed',
+            bottom: '0',
+            left: '0',
+            zIndex: 1000,
+            width: '100%',
+          };
+        } else {
+          this.style = {}; // reset style for other routes
+        }
+      }
+    });
+  }
 }
