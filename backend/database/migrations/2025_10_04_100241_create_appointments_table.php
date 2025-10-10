6d04795edb6b269e28d49f125c1204db0f97553d
Abdallah Taha <abdallahkhoder59@gmail.com>
2025-10-11 00:19:20 +0300

    removing vendor_id column from table appointments

--- backend/database/migrations/2025_10_04_100241_create_appointments_table.php
+++ backend/database/migrations/2025_10_04_100241_create_appointments_table.php
@@ -13,7 +13,6 @@ return new class extends Migration {
         Schema::create('appointments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('client_id')->constrained('users');
-            $table->foreignId('vendor_id')->constrained('vendors')->cascadeOnDelete();
             $table->foreignId('category_id')->constrained('categories');
             $table->date('appointment_date');
             $table->string('location');
